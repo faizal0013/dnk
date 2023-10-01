1ae761d46fe32e8cba58f7bb3c006aa8d9e0d289
Faizal Khalifa <faizalfkhalifa786@gmail.com>
2023-10-01 17:41:06 +0530

    remove unneed Comment

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
 
-from product_inventorys.serializers import ProductInventorySerializer
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
-    # product_inventorys = ProductInventorySerializer(many=True)
 
     class Meta:
         model = Product
